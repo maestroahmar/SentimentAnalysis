1a1ec7762ac18c9ba4a190e3d4defa1c8e017e79
maestroahmar <solemn_martinian@yahoo.com>
2017-02-27 12:15:22 +0530

    Add files via upload

--- google_search.php
+++ google_search.php
@@ -1,20 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
-</head>
-<body>
 
-<?php    
+<?php 
+
+//Using the themoviedb API to collect information for Now Playing Movies.   
 $url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=3a92fa1ff3261395ad9bd44c7cc95519&language=en-US&page=1&region=in';
 
+    //Function to convert the data format
     function get_json_format($u)
     {
         $json = file_get_contents($u);
         $data = json_decode($json);
         return $data;
     }
-
+    //Function to create hashtags for the Movie and Lead Actor, The hashtags will be used to search
     function create_hashtag($h)
     {
     $h = str_replace(' ', '', $h);
@@ -30,48 +27,70 @@ $url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=3a92fa1ff3261395a
 	
     
 
-    $data = get_json_format($url);
-    $arr = $data->results;
-    $count_now_playing = count($data->results);
-    echo $count_now_playing;
-    $title = array();
-    $over = array();
-    $poster = array();
-    $movie_file = fopen("movie_names.txt", "w+");
-    for ($x = 0; $x <$count_now_playing; $x++)
-    {
-    	
-	 $title[$x] = $data->results[$x]->title;
-	$movie_id = $data->results[$x]->id;
-   echo $title[$x]."\n";
-   echo $movie_id."\n";
-   $credits_movie = "https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=3a92fa1ff3261395ad9bd44c7cc95519";
-       $credits_movie = str_replace("{movie_id}", $movie_id, $credits_movie);
-       $json_cast = file_get_contents($credits_movie);
-       $cast = json_decode($json_cast);
-       $actor=$cast->cast[0]->name;
-       $actor = create_hashtag($actor);
-      $title[$x] = create_hashtag($title[$x]);
-        echo $title[$x]."\n".$actor;
-    $movie_name = $title[$x];
-    $title[$x] = $title[$x]."\n";
-    exec("Rscript words.r $movie_name $actor",$call_file);
-    print_r($call_file);
+$data = get_json_format($url);
+$arr = $data->results;
+$count_now_playing = count($data->results);
+$title = array();
+$over = array();
+$poster = array();
+$movie_file = fopen("movie_names.txt", "w+");
 
-    fwrite($movie_file, $title[$x]);
-
+for ($x = 0; $x <$count_now_playing; $x++)
+{
 	
-	$over[$x]=$data->results[$x]->overview;
+$title[$x] = $data->results[$x]->title;
+$movie_id = $data->results[$x]->id;
+echo "Movie Name --> ".$title[$x]."\n";
+$credits_movie = "https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=3a92fa1ff3261395ad9bd44c7cc95519";
+$credits_movie = str_replace("{movie_id}", $movie_id, $credits_movie);  
+$json_cast = file_get_contents($credits_movie);
+$cast = json_decode($json_cast);
+$actor=$cast->cast[0]->name;
+$actor = create_hashtag($actor);
+$title[$x] = create_hashtag($title[$x]);
+echo $title[$x]."\n".$actor;
+$movie_name = $title[$x];
+$title[$x] = $title[$x]."\n";
+exec("Rscript words.r $movie_name $actor",$call_file);
+print_r($call_file);
 
-	$poster[$x] = $data->results[$x]->poster_path;
+fwrite($movie_file, $title[$x]);
+
+
+$over[$x]=$data->results[$x]->overview;
+
+$poster[$x] = $data->results[$x]->poster_path;
 }
-   
+
+//Ratings of the movies calculated
+$rating_file =  file_get_contents("ratings.txt");
+$s = "";
+$movies_ratings = array();
+for($i=0; $i<strlen($rating_file); $i++)
+{   
+
+    if(!is_numeric($rating_file[$i]) && $rating_file[$i] != '-')
+    {   
+        $s = (integer)$s;
+        array_push($movies_ratings,$s);
+        $s = "";
+    }
+    else
+    {
+        $s = $s.$rating_file[$i];
+    }
+}
+
+rsort($movies_ratings);
 
 
-    
+
+
+
+
+
+
+
 ?>
 
 
-
-</body>
-</html>
--- words.r
+++ words.r
@@ -1,28 +1,47 @@
-library(twitteR)
+library(twitteR) 
 s <- 0
 smiley_count <- 0
 words_count <- 0
+neg <- 0
+pos <- 0
 consumer_key <- "iS71TIxdgtjMn90IhtqMKqscn"
 consumer_secret <- "rmFMbW3BnYQK82Sk7orrlE1jdrOi54LWtd851x7GXgxfZELza8"
 access_token <- "380908509-EYQ0TQqKY7xnoRD1wboeYaQsHLYWnUPFPgCRfGhc"
 access_secret <- "2XWL29B7EIE3nZru5av9t9Zg7Sb6bl50z1jNaASQ0b6jo"
-args <- commandArgs(TRUE)
+#credentials for accessing twitter tweets
+args <- commandArgs(TRUE) 
 names_movies <- as.integer(args[1])
 actor <- as.integer(args[2])
+#movie name and actor input from php file
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-tweets <- searchTwitter(names_movies,n=500, resultType = "recent")
-tweets <- strip_retweets(tweets)
+#setting up twitter authentication
+tweets <- searchTwitter(names_movies,n=100, resultType = "recent")
+#search for particular movie along with actor
+#tweets <- strip_retweets(tweets)
+#removing the retweets which are repeated more than once.
+
+#Cleaning Data
 tweet_txt = sapply(tweets, function(x) x$getText())
+#Getting tweets in text format.
 tweet_txt = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", tweet_txt)
+#removing via mentions, RT.
 tweet_txt = gsub("@\\w+", "", tweet_txt)
+#removing the @ mentions
 tweet_txt = gsub("[[:digit:]]", "", tweet_txt)
+#removing unnecessary numbers
 tweet_txt = gsub("http\\w+", "", tweet_txt)
+#removing any unnecessary links
 tweet_txt = gsub("^\\s+|\\s+$", "", tweet_txt)
 tweet_txt = gsub("[ \t]{2,}", "", tweet_txt)
+#removing & symbols
 tweet_txt = gsub("amp", "", tweet_txt)
+
+#reading the words along with their weights.
 val <- read.table("tweets_val.txt",header=F,sep="\t",quote="",col.names=c("term","score"))
+#reading smiley symbols along with their weights
 smi <- read.table("smileys.txt",col.names=c("smiley","score"))
- 
+
+#searching for words to match with the list provided.
 for(i in tweet_txt)
 {
 tw <- strsplit(i," ")[[1]]
@@ -32,9 +51,20 @@ for(j in tw)
 	{
 
 		s <- s + smi[which(smi$smiley == j),2]
+		#add the weight collected from the words
 		smiley_count = smiley_count + 1
+
+		if(smi[which(smi$smiley == j),2] < 0)
+		{
+			neg <- neg + 1;
+		}
+		else
+		{
+			pos <- pos + 1;
+
+		}
+
 		
-	print(i)
 	}
 	
 	if(j %in% val[,1])
@@ -42,7 +72,15 @@ for(j in tw)
 
 	s <- s + val[which(val$term == j),2]
 	words_count = words_count + 1
-	
+	if(val[which(val$term == j),2] < 0)
+		{
+			neg <- neg + 1;
+		}
+		else
+		{
+			pos <- pos + 1;
+
+		}
 	
 	}
 
@@ -50,7 +88,8 @@ for(j in tw)
 }
 
 }
-write.table(c(names_movies,s),file='ratings.csv',append=TRUE)
+#write the value to a text file.
+write(s,file='ratings.txt',append=TRUE)
 print(s)
 
 
