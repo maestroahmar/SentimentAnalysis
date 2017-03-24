0e4a6f7de4d9dbd7d8a8c890ebbf6d1b37063b6b
maestroahmar <solemn_martinian@yahoo.com>
2017-03-24 15:51:08 +0530

    Add files via upload

--- google_search.php
+++ google_search.php
@@ -4,22 +4,28 @@
 //Using the themoviedb API to collect information for Now Playing Movies.   
 $url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=3a92fa1ff3261395ad9bd44c7cc95519&language=en-US&page=1&region=in';
 
-    //Function to convert the data format
+    //Function to convert the data format in JSON
     function get_json_format($u)
     {
-        $json = file_get_contents($u);
-        $data = json_decode($json);
+        $json = file_get_contents($u);          //Getting the HTML contents of the webpage.
+        $data = json_decode($json);             //The HTML content is converted into JSON format which is easy to comprehend. 
         return $data;
     }
     //Function to create hashtags for the Movie and Lead Actor, The hashtags will be used to search
     function create_hashtag($h)
     {
-    $h = str_replace(' ', '', $h);
+    $h = str_replace(' ', '', $h);      //removing the unwanted spaces from the movie name
     $h = strtolower($h);
+    //lower case conversion  to provide ease in searching for the movie via R. Twitter Search via R is case sensitive.
     $h = preg_replace("/:[[:alnum:]]*/","", $h);
+    //removing the unwanted tagline of the movie that is mined from the DB. This will result in the shortening of the name, this is known as Tokenization.The asterix here denotes the use of Regular Expressions which find any sequence that is alpha numeric.
+
     $h =  preg_replace("/[^A-Za-z0-9\-]/", "", $h);
+    //Any wanted keyword that starts will be replaced
     $h =  str_replace("-", "", $h);
+    //Replacing unwanted hyphen.
     $h = "#".$h;
+    //concatinating # before the movie to make the search word more existing and is easy to find.
     return $h;
     }
 
@@ -28,53 +34,66 @@ $url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=3a92fa1ff3261395a
     
 
 $data = get_json_format($url);
+//user-defined function call  to get JSON format ready
 $arr = $data->results;
-$count_now_playing = count($data->results);
+// accessing the results that are produced using referential structure.
+$count_now_playing = count($data->results);         //count the number of upcoming movies by counting the distinct number of movies that are present in DB.
+
 $title = array();
+//Intializing an array that is used to store the names of the movies.
 $over = array();
+//Intializing an array that is used to store the overview/small description of the movies.The description will be shown the main home page of site.
 $poster = array();
-$movie_file = fopen("movie_names.txt", "w+");
+//Intializing an array that is used to store the url of the poster of movies, the posters are provided as web .jpg format and can be downloaded. It will be displayed in the div property of the site.
+#$movie_file = fopen("movie_names.txt", "w+");
+
 $movie_name = fopen("movies.txt","w+");
+//Opening a new file/existing file which will contain the names of all the movies that  are being named from the Movie DB.
 for ($x = 0; $x <$count_now_playing; $x++)
+    //Iterating with the number of the JSON objects as the names of the movies.
 {
 	
 $title[$x] = $data->results[$x]->title;
+//Extracting/Mining the title of the movie JSON object from the returned object. 
 $movie_id = $data->results[$x]->id;
+//Extracting/Mining the title of the movie JSON object from the returned object. The movie ID is unique, which is used further to call API to extract more data about the movie.
 echo "Movie Name --> ".$title[$x]."\n  Movie ID ---->".$movie_id;
+//Debugging purposes console output.
 $credits_movie = "https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=3a92fa1ff3261395ad9bd44c7cc95519";
-$credits_movie = str_replace("{movie_id}", $movie_id, $credits_movie);  
+//new url for calling a new API with different data.q
+$credits_movie = str_replace("{movie_id}", $movie_id, $credits_movie); 
+//Replacing the general/generic url with the movie Id, dynamically every time the loop iterates. 
 $json_cast = file_get_contents($credits_movie);
+//user-defined function call  to get JSON format ready. This API call is reponsible for getting the information associated with the movie such as Protogonist, Music Director.
 $cast = json_decode($json_cast);
+//The HTML content is converted into JSON format which is easy to comprehend. 
 $actor=$cast->cast[0]->name;
+//The 'cast' JSON object array contains the names of actors who played their part in the movie.
 $actor = create_hashtag($actor);
+//calling the user defined method to get the name of the actor in a format so that they get hashtags names.
 #fwrite($movie_name, $title[$x]);
 $title[$x] = create_hashtag($title[$x]);
+//calling the user defined method to get the name of the movie in a format so that they get hashtags names.
 echo $title[$x]."\n".$actor;
+//Debugging purposes console output.
 $movie_name = $title[$x];
 $title[$x] = $title[$x]."\n";
 exec("Rscript words.r $movie_name $actor",$call_file);
+//Calling the R file which is used to fetch data from Twitter, and make sentiment calculations for the same. The name of the movie and actor are passed as command line arguments to the R file execution.
 print_r($call_file);
+//Debugging purposes console output.
 
-fwrite($movie_file, $title[$x]);
+#fwrite($movie_file, $title[$x]);
 
 $over[$x]=$data->results[$x]->overview;
-
+//The 'data' JSON object array contains the overview/summary who played their part in the movie and the plot of movie in short.
 $poster[$x] = " http://image.tmdb.org/t/p/w185".$data->results[$x]->poster_path;
-
+//The JSOM returns the image link which is relative, in order to make it an absolute link it will be required that a hard coded link is concatenated with the poster url.
 echo "\n".$poster[$x]."\n";
-
+//Debugging purposes console output.
 
 
 }
-
-//Ratings of the movies calculated
-
-
-
-
-
-
-
 
 
 ?>
--- words.r
+++ words.r
@@ -1,4 +1,6 @@
 library(twitteR) 
+#twitteR is an R package which provides access to the Twitter API. Most functionality of the API is supported, with a bias towards API calls that are more useful in data analysis as opposed to daily interaction.
+
 s <- 0
 smiley_count <- 0
 words_count <- 0
@@ -6,27 +8,37 @@ fneg <- 0
 fpos <- 0
 neg <- 0
 pos <- 0
+# Instance/Platform variables that are responsible for holding the values of certain parameters of the calculation
 consumer_key <- "iS71TIxdgtjMn90IhtqMKqscn"
+#Consumer keys are also known as API keys. You'll also need your consumer secret. To make API calls, you'll also need an access token and access token secret, collectively called an "oauth token" -- they represent a user making a request
 consumer_secret <- "rmFMbW3BnYQK82Sk7orrlE1jdrOi54LWtd851x7GXgxfZELza8"
+#onsumer secret is the client password that is used to authenticate with the authentication server, which is a Twitter/Facebook/etc. server that authenticates the client. Access token is what is issued to the client once the client successfully authenticates itself (using the consumer key & secret).
 access_token <- "380908509-EYQ0TQqKY7xnoRD1wboeYaQsHLYWnUPFPgCRfGhc"
+#An access token is an object that describes the security context of a process or thread. The information in a token includes the identity and privileges of the user account associated with the process or thread. ... The security identifier (SID) for the user's account. SIDs for the groups of which the user is a member.
 access_secret <- "2XWL29B7EIE3nZru5av9t9Zg7Sb6bl50z1jNaASQ0b6jo"
+#Access token is what is issued to the client once the client successfully authenticates itself (using the consumer key & secret). This access token defines the privileges of the client (what data the client can and cannot access). Now every time the client wants to access the end-user's data, the access token secret is sent with the access token as a password (similar to the consumer secret).
 #credentials for accessing twitter tweets
 args <- commandArgs(TRUE) 
+#Command Line Arguments for getting the name of the movie that is being passed in the PHP file (base) which is stored here in a variable.
 names_movies <- as.integer(args[1])
 actor <- as.integer(args[2])
+#The received parameter is an array moreover a list that has the name of the movie and actor as [1] and [2] position.
 #movie name and actor input from php file
+search_key <- paste(names_movies,actor,sep=' ')
+#Paste function is used to concatenate the names of the movie and actor to search the keyword on  Twitter. 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-#setting up twitter authentication
-tweets <- searchTwitter(names_movies,n=100, resultType = "recent")
-#search for particular movie along with actor
+#setting up twitter authentication with  all the parameters the session is setup for secure authentication.
+tweets <- searchTwitter(search_key,n=100, resultType = "recent")
+#search for particular movie along with actor with this function, the function contains atleast 2 variables, one is the text that is searched on the Twitter DB, other one is the number of the tweets that should be searched for
+#NOTE- If the number of mentions do not match  the number provided or is less than the number, then complete search is not done, it is stopped.
 #tweets <- strip_retweets(tweets)
 #removing the retweets which are repeated more than once.
 
 #Cleaning Data
 tweet_txt = sapply(tweets, function(x) x$getText())
-#Getting tweets in text format.
+#Getting tweets in text format.Instead of using for loop  to iterate through all tweets, we use apply functions so that a common operation is applied to  all.
 tweet_txt = gsub("(RT|via)((?:\\b\\W*@\\w+)+)", "", tweet_txt)
-#removing via mentions, RT.
+#removing via mentions, RT via Regular Expressions.
 tweet_txt = gsub("@\\w+", "", tweet_txt)
 #removing the @ mentions
 tweet_txt = gsub("[[:digit:]]", "", tweet_txt)
@@ -115,18 +127,18 @@ print(neg/fneg)
 #write the value to a text file.
 if (fpos > fneg)
 {
-fin <- 5*(1-((exp((-1)*(fpos/pos)))*(exp((fneg/neg))))) + ((fpos-fneg)/(fpos+fneg)) 
+fin <- 10*(1-((exp((-1)*(fpos/pos)))*(exp((fneg/neg))))) + ((fpos-fneg)/(fpos+fneg)) 
 }
 if(fpos < fneg)
 {
-fin <- 5*(1-((exp((-1)*(fpos/pos)))*(exp((fneg/neg))))) - ((fpos-fneg)/(fpos+fneg)) 
+fin <- 10*(1-((exp((-1)*(fpos/pos)))*(exp((fneg/neg)))) - ((fpos-fneg)/(fpos+fneg))) 
 
 }
 print(fin)
-write(fin,file='ratings.txt',append=TRUE)
-write(fpos,file='fpositive.txt',append=TRUE)
-write(fneg,file='fnegative.txt',append=TRUE)
+struct <- list(pos,fpos,neg,fneg,fin)
+write.table(struct,file='complete.csv',sep=',',append=T,col.names=F,row.names=F)
 
 
 
 
+1
